c5cef87c84b5824617da3eed9e31d29ab21ba009
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-04-08 16:38:59 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// IMC DATA
+function alterarCor(idClicado) {
+    var masculino = document.getElementById('masculinoid');
+    var feminino = document.getElementById('femininoid');
+
+    if (idClicado === 'masculinoid') {
+        masculino.style.backgroundColor = 'blue';
+        feminino.style.backgroundColor = '';
+    } else if (idClicado === 'femininoid') {
+        feminino.style.backgroundColor = 'deeppink';
+        masculino.style.backgroundColor = '';
+    }
+}
+
 const data = [
     {
       min: 0,
